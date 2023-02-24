cab444f9a088b1f81d8e0e168e53daa6cc5c0438
FaustVX <jodu035@gmail.com>
2023-02-24 09:46:42 +0100

    cp program

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 sudo systemctl stop flux
 dotnet publish -c Release --sc -a arm --os linux -p:PublishSingleFile=true
+sudo cp bin/Release/net7.0/linux-arm/publish/flux /usr/bin
 sudo systemctl start flux
