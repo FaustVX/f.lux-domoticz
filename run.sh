69ea605deaf0cfcffdbfc20531c11c5e3a648487
FaustVX <jodu035@gmail.com>
2023-02-24 14:12:27 +0100

    Added `echo` in `run.sh`

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+echo stop service
 sudo systemctl stop flux
+echo build project
 FOLDER=$(dotnet publish -c Release --sc -a arm --os linux -p:PublishSingleFile=true | tail -n 1 | awk '{print $3}')
-echo $FOLDER
+echo copy to /usr/bin
 sudo cp $FOLDER./flux /usr/bin
+echo start service
 sudo systemctl start flux
